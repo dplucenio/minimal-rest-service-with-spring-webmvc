4c3880baf267f7e29437e99529553d6ab468af6f
Daniel Plucenio <dplucenio@gmail.com>
2020-08-17 18:38:54 -0300

    remove commented code

--- src/main/java/io/plucen/AppConfiguration.java
+++ src/main/java/io/plucen/AppConfiguration.java
@@ -13,17 +13,4 @@ public class AppConfiguration {
   public ObjectMapper getObjectMapper() {
     return new ObjectMapper();
   }
-
-  //  @Getter private static final StudentRepository studentRepository = new
-  // MemoryStudentRepository();
-  //
-  //  @Getter
-  //  private static final StudentService studentService = new StudentService(studentRepository);
-  //
-  //  @Getter
-  //  private static final StudentsController studentsController =
-  //      new StudentsController(studentService);
-  //
-  //  @Getter private static final DashboardController dashBoardController = new
-  // DashboardController();
 }
