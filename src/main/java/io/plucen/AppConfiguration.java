1390228239ec4358154bfa2920778da5c10d385c
Daniel Plucenio <dplucenio@gmail.com>
2020-08-17 18:38:54 -0300

    Remove unused ObjectMapper Bean

--- src/main/java/io/plucen/AppConfiguration.java
+++ src/main/java/io/plucen/AppConfiguration.java
@@ -1,7 +1,5 @@
 package io.plucen;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +7,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class AppConfiguration {
-
-  @Bean
-  public ObjectMapper getObjectMapper() {
-    return new ObjectMapper();
-  }
-}
+public class AppConfiguration {}
